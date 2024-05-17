7ddefbc5346c990205dc06494184e39373339145
leonevers00 <160394955+leonevers00@users.noreply.github.com>
2024-05-17 12:41:16 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    for (let index = 0; index < 3; index++) {
+        music.playMelody("E A F G D E F G ", 250)
+    }
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("E A F G D E F G ", 300)
+    }
+    for (let index = 0; index < 5; index++) {
+        music.playMelody("E A F G D E F G ", 350)
+    }
+})
